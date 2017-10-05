5ce3d210b31f0b58dde1c7a133341fa93cd9a6c0
codd <cesardelgado73@hotmail.com>
2017-10-04 23:31:04 -0400

    Add EntityBase Class

---
+++ LearningDDDwithCSharp.Infraestructure.Common/Domain/BusinessRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningDDDwithCSharp.Infraestructure.Common.Domain
+{
+    class BusinessRule
+    {
+        public BusinessRule(string ruleDescription)
+        {
+            RuleDescription = ruleDescription;
+        }
+
+        public string RuleDescription { get; set; }
+    }
+}
---
+++ LearningDDDwithCSharp.Infraestructure.Common/Domain/EntityBase.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace LearningDDDwithCSharp.Infraestructure.Common.Domain
+{
+    public abstract class EntityBase<T>
+    {
+        public T Id { get; set; }
+
+        private List<BusinessRule> _brokenRules = new List<BusinessRule>();
+        public override bool Equals(object entity)
+        {
+
+            return entity != null && entity is EntityBase<T>
+                   && this == (EntityBase<T>) entity;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+
+        public static bool operator ==(EntityBase<T> entity1, EntityBase<T> entity2)
+        {
+            if ((object) entity1 == null && (object) entity2 == null)
+            {
+                return true;
+            }
+            if ((object) entity1 == null || (object) entity2 == null)
+            {
+                return false;
+            }
+            if (entity1.Id.ToString() == entity2.Id.ToString())
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public static bool operator !=(EntityBase<T> entity1, EntityBase<T> entity2)
+        {
+            return (!(entity1 == entity2));
+        }
+
+        protected abstract void Validate();
+    }
+}
